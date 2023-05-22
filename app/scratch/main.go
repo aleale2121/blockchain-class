714ccbe134e3b67ac48f72caa4ace65a3eba0684
alefew <alefewyimer4@gmail.com>
2023-05-22 10:04:24 +0300

    transaction type 1

--- app/scratch/main.go
+++ app/scratch/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"encoding/hex"
 	"encoding/json"
 	"fmt"
+	"math/big"
 
 	"github.com/ethereum/go-ethereum/common/hexutil"
 	"github.com/ethereum/go-ethereum/crypto"
@@ -15,10 +17,15 @@ type Tx struct {
 }
 
 func main() {
+	fmt.Println("...........")
 	err := run()
 	if err != nil {
+		fmt.Println("...........")
+		fmt.Println(err)
 		panic(err)
 	}
+	fmt.Println("...........")
+
 }
 
 func run() error {
@@ -29,7 +36,7 @@ func run() error {
 	}
 
 	tx := Tx{
-		FromID: "9f332e3700d8fc2446eaf6d15034cf96e0c2745e40353deef032a5dbf1dfed93",
+		FromID: "0xF01813E4B85e178A83e29B8E7bF26BD830a25f32",
 		ToID:   "Aaron",
 		Value:  1000,
 	}
@@ -38,6 +45,7 @@ func run() error {
 	if err != nil {
 		return fmt.Errorf("unable to marshal: %w", err)
 	}
+
 	stamp := []byte(fmt.Sprintf("\x19Ardan Signed Message:\n%d", len(data)))
 
 	v := crypto.Keccak256(stamp, data)
@@ -59,7 +67,7 @@ func run() error {
 	fmt.Println("PUB:", crypto.PubkeyToAddress(*publicKey).String())
 
 	tx = Tx{
-		FromID: "9f332e3700d8fc2446eaf6d15034cf96e0c2745e40353deef032a5dbf1dfed93",
+		FromID: "0xF01813E4B85e178A83e29B8E7bF26BD830a25f32",
 		ToID:   "Frank",
 		Value:  250,
 	}
@@ -91,7 +99,7 @@ func run() error {
 	// over the wire =======================================
 
 	tx2 := Tx{
-		FromID: "9f332e3700d8fc2446eaf6d15034cf96e0c2745e40353deef032a5dbf1dfed93",
+		FromID: "0xF01813E4B85e178A83e29B8E7bF26BD830a25f32",
 		ToID:   "Frank",
 		Value:  250,
 	}
@@ -111,5 +119,27 @@ func run() error {
 	}
 
 	fmt.Println("PUB:", crypto.PubkeyToAddress(*publicKey).String())
+
+	V, r, s, err := ToVRSFromHexSignature(hexutil.Encode(sig2))
+	if err != nil {
+		return fmt.Errorf("unable to VRS: %w", err)
+	}
+	fmt.Println("V|R|S", V, r, s)
+
 	return nil
+}
+
+// ToVRSFromHexSignature converts a hex representation of the signature into
+// its R, S and V parts.
+func ToVRSFromHexSignature(sigStr string) (v, r, s *big.Int, err error) {
+	sig, err := hex.DecodeString(sigStr[2:])
+	if err != nil {
+		return nil, nil, nil, err
+	}
+
+	r = big.NewInt(0).SetBytes(sig[:32])
+	s = big.NewInt(0).SetBytes(sig[32:64])
+	v = big.NewInt(0).SetBytes([]byte{sig[64]})
+
+	return v, r, s, nil
 }
